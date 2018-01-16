0fdc28912e3533d93f578f0638caca079f3b63c2
Abel Romero Perez <abel@abelromero.com>
2018-01-16 19:00:29 +0100

    updated licenses on files

--- rootkit.v1-2.c
+++ rootkit.v1-2.c
@@ -1,20 +1,21 @@
 /*
- * ARP RootKit v1.0, a simple rootkit for the Linux Kernel.
- * 
- * Copyright 2018 Abel Romero Pérez aka D1W0U <abel@abelromero.com>
+ * Copyright (c) 2018 Abel Romero Pérez aka D1W0U <abel@abelromero.com>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of ARP RootKit.
+ *
+ * ARP RootKit is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Foobar is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 char code_start = 0;
