2124eafd49a22fa094a4b160ca3190be439ce468
Abel Romero Perez <abel@abelromero.com>
2018-02-08 03:06:55 +0100

    Finally I got the way to get the offsets for int (*init)(void) and void (*exit)(void) from struct module. Now we can patch the .ko to load on any 4.x kernel (version tested). Remaining the development of rela-patch.c and patch-lkm.py.

--- find-rela.py
+++ find-rela.py
@@ -6,22 +6,29 @@ import struct
 from capstone_python import *
 from capstone_python.x86 import *
 
-def find_sct(vmlinux, secdata):
-	cmd = "objcopy --dump-section .rodata=" + secdata + " " + vmlinux
-	if os.system(cmd):
-		os.remove(secdata)
-		return -1
+__NR_delete_module = 176
+
+def get_text_offsets(vmlinux):
 	out = subprocess.check_output(["readelf", "-t", "--wide", vmlinux])
 	#.text
-    #   PROGBITS        ffffffff81000000 200000 c031d1 
+	#   PROGBITS        ffffffff81000000 200000 c031d1
 	pattern = "\.text\n\s*?PROGBITS\s*?([a-z0-9]{16}) \d+ ([a-z0-9]{0,16}?) "
 	regex = re.compile(pattern)
 	out = out.decode('utf-8')
 	match = regex.findall(out)
 	code_start = int(match[0][0], 16)
 	code_end = code_start + int(match[0][1], 16)
+	return (code_start, code_end)
+
+def find_sct(vmlinux, code_start, code_end):
+	secdata = vmlinux + ".data"
+	cmd = "objcopy --dump-section .rodata=" + secdata + " " + vmlinux
+	if os.system(cmd):
+		os.remove(secdata)
+		return -1
 	f = open(secdata, 'rb')
 	c = 0
+	sct = []
 	while True:
 		data = f.read(8)
 		if data == '' or len(data) < 8:
@@ -30,36 +37,61 @@ def find_sct(vmlinux, secdata):
 		data = data[0] 
 		if data >= code_start and data <= code_end:
 			c += 1
+			sct.append(data)
 			#if (c >= 500):
 			#	print("%d found sct!" % c)
 		else:
 			if c > 500 and c < 600:
 				print("Found sct! %d syscalls" % c)
+				break
 			c = 0
+			sct = []
 	f.close()
 	os.remove(secdata)
-	return 0
+	return sct
 
 vmlinuz = sys.argv[1]
 vmlinux = "vmlinux"
 cmd = "./extract-vmlinux " + vmlinuz + " > " + vmlinux + " 2>/dev/null"
 if os.system(cmd):
 	sys.exit(-1)
-secdata = vmlinux + ".data"
-find_sct(vmlinux, secdata)
-sys.exit(0)
-code = vmlinux + ".code"
-cmd = "objcopy --dump-section .text=" + code + " " + vmlinux
+
+def find_rela_offsets(vmlinux):
+	(code_start, code_end) = get_text_offsets(vmlinux)
+	sct = find_sct(vmlinux, code_start, code_end)
+	off = sct[__NR_delete_module] - code_start
+	print ("sys_delete_module at offset .text+0x%x" % off)
+	seccode = vmlinux + ".code"
+	cmd = "objcopy --dump-section .text=" + seccode + " " + vmlinux
+	if os.system(cmd):
+		os.remove(vmlinux)
+		sys.exit(-1)
+	f = open(seccode, "rb")
+	f.seek(off, os.SEEK_SET)
+	code = f.read(500)
+	f.close()
+	md = Cs(CS_ARCH_X86, CS_MODE_64)
+	md.syntax = CS_OPT_SYNTAX_ATT
+	insts = ""
+	for i in md.disasm(code, off):
+		insts += i.mnemonic + "\t" + i.op_str + "\n"
+		#print (inst)
+		#print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
+	#print (insts)
+	pattern = "cmpq\s+?\$0, (\S+)?\(.*?\)\nje\s*?.*?\ncmpq\s+?\$0, (\S+?)?\(.*?\)\n"
+	regex = re.compile(pattern)
+	match = regex.findall(insts)
+	#print (match)
+	init = int(match[0][0], 16)
+	exit = int(match[0][1], 16)
+	os.remove(seccode)
+	return init, exit
+
+vmlinuz = sys.argv[1]
+vmlinux = "vmlinux"
+cmd = "./extract-vmlinux " + vmlinuz + " > " + vmlinux + " 2>/dev/null"
 if os.system(cmd):
-	os.remove(vmlinux)
-	sys.exit(-1)
-
-f = open(code, "rb")
-CODE = f.read()
-f.close()
-
-md = Cs(CS_ARCH_X86, CS_MODE_64)
-for i in md.disasm(CODE, 0):
-	print("0x%x:\t%s\t%s" %(i.address, i.mnemonic, i.op_str))
-
+    sys.exit(-1)
+init, exit = find_rela_offsets(vmlinux)
+print ("init = %d\nexit = %d" % (init, exit))
 os.remove(vmlinux)
